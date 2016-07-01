5b8976fbae1eecc03737d5078bfeaae2e88779b0
cosine <urillx@gmail.com>
2016-07-01 16:18:30 -0400

    updated for clarity and robustness

--- ArduinoPid1.ino
+++ ArduinoPid1.ino
@@ -1,138 +1,107 @@
+#include <Arduino.h>
 #include <Servo.h>
 #include <Encoder.h>
 
-
 Encoder myEnc(2, 3);
-const int Pot1 = A0;
-const int Pot2 = A1;
-const int Pot3 = A2;
-double lastError = 0;
-double errorSum = 0;
-double motor_input; 
-double act_pos = 0;
-const int theta_des = 50;
-int kp = 0;
-int ki = 0;
-int kd = 0;
-int errordiff = 0;
-int motor_init = 90;
-int error1 = 0; 
-long oldPosition  = 0;
-long unsigned int newtime;
-long unsigned int oldtime;
-int motorvar = 90;
-int degree = 0;
-int minServo = 30;
-int maxServo = 150;
-long unsigned int lastTime = 0;
-double totalerror=0;
 Servo myservo;
 
+// pin config
+const int pin_pot_p = A0;
+const int pin_pot_i = A1;
+const int pin_pot_d = A2;
+const int pin_servo = 11;
+
+// set the control interval
+const int control_interval = 10; // ms
+// set how many digits serial prints the floats
+const int print_precision = 6; // digits
+
+// set the preset setpoints and time between step
+const int step_interval = 1000; // ms
+const double setpoint_1 = 0; // rad
+const double setpoint_2 = M_PI/6.; // rad
+
+// persistent variables for PID control
+double setpoint = 0; // rad
+unsigned long last_control_time = 0;
+double last_error = 0;
+double sum_error = 0;
+// persistent variables for stepping
+unsigned long last_step_time = 0;
+boolean step_control = 0;
+
+
 void setup() {
-  // put your setup code here, to run once:
-    pinMode(Pot1, INPUT);
-    pinMode(Pot2, INPUT);
-    Serial.begin(115200);
-    myservo.attach(11,1000,2000);
-    myEnc.write(0);
+  pinMode(pin_pot_p, INPUT);
+  pinMode(pin_pot_i, INPUT);
+  pinMode(pin_pot_d, INPUT);
+  pinMode(pin_servo, OUTPUT);
+
+  Serial.begin(115200);
+  myservo.attach(11, 1000, 2000);
+  myEnc.write(0);
+
+  // home
+  myservo.write(75);
+  boolean moving = 1;
+  long last_encoder = myEnc.read();
+  while (moving) {
+    delay(300);
+    long current_encoder = myEnc.read();
+    moving = abs(current_encoder - last_encoder) > 10 ? 1 : 0;
+    last_encoder = current_encoder;
+  }
+  myEnc.write(-1100);
 }
 
 void loop() {
-    long newPosition = myEnc.read();
-    if (newPosition > oldPosition+10) {
-      oldPosition = newPosition;
-      degree++;
-     // Serial.println(degree);
-    }
-    if (newPosition <oldPosition-10){
-      oldPosition = newPosition;
-      degree--;
-      //Serial.println(degree);
-    }
-    
-    
-//    newtime = millis();
-//    if (newtime>oldtime+1000){
-//      if (motorvar == 116){ 
-//        motorvar = 65;}
-//      else{ 
-//        motorvar = 116;}
-//      oldtime = newtime;
-//    
-//      }
-//    myservo.write(motorvar);
-    
-    
+  unsigned long now = millis();
+  if (now - last_control_time >= control_interval) {
+    // read the pot
+    double kp = analogRead(pin_pot_p) * 0.1;
+    double ki = analogRead(pin_pot_i) * 0.01;
+    double kd = analogRead(pin_pot_d) * 0.01;
 
-  if((millis()-lastTime)>=10){
-  pidControl();
-  Serial.print(degree);
-  Serial.print(",");
-  Serial.print(theta_des);
-  Serial.print(",");
-  Serial.print(totalerror);
-  Serial.print(",");
-  Serial.print("p;");
-  Serial.print(ReadPot1());
-  Serial.print(",");
-  Serial.print("i;");
-  Serial.print(ReadPot2());
-  Serial.print(",");
-  Serial.print("d;");
-  Serial.print(ReadPot3());
-  Serial.print("\n");
-  lastTime=millis();
+    // read the current position from encoder
+    long current_position_encoder_tick = myEnc.read();
+    // convert it to radians
+    double current_position = current_position_encoder_tick * 2 * M_PI / 3600.; // rad
+
+    // calculate error
+    double error = current_position - setpoint;
+    // calculate derivative of error
+    double diff_error = error - last_error;
+    // integrate error
+    sum_error += error;
+
+    // calculate how many volts should be applied to motor terminals
+    double control_input = (kp*error + ki*sum_error + kd*diff_error)*-1;
+
+    // convert motor terminal voltage to servo signal
+    int servo_signal = map(control_input, -5., 5., 70, 90);
+    // actually drive the motor
+    myservo.write(servo_signal);
+
+    last_error = error;
+    last_control_time = now;
+
+    Serial.print(current_position, print_precision);
+    Serial.print(',');
+    Serial.print(setpoint, print_precision);
+    Serial.print(',');
+    Serial.print(control_input, print_precision);
+    Serial.print(';');
+    Serial.print(kp, print_precision);
+    Serial.print(',');
+    Serial.print(ki, print_precision);
+    Serial.print(',');
+    Serial.print(kd, print_precision);
+    Serial.print('\n');
   }
-}
 
-void pidControl(){
-    kp = ReadPot1();
-    ki = ReadPot2();
-    kd = ReadPot3();
-    
-    error1 = theta_des - degree;
-    errordiff = degree - lastError;
-    
-    errorSum+=errordiff;
-    
-    if(errordiff<0.5){
-    errordiff=0;
-    }
-    
-    totalerror = motor_init + (kp * error1) + (kd * errordiff) + (ki*errorSum);
-    motor_input = totalerror;
-    lastError = degree;
-
-//  if (degree<0){//t
-//  Serial.println("here");
-//  motor_input = constrain(motor_input,90-(degree)*kp-(kd * errordiff),maxServo);
-//  motor_input = constrain(motor_input,minServo,maxServo);
-//  }else 
-//  if (degree<15){//t
-//  motor_input = constrain(motor_input,90,maxServo);
-//  } else if (degree>85){//t
-//  motor_input = constrain(motor_input,minServo,90);
-//  } else {
-//  motor_input = constrain(motor_input,minServo,maxServo);
-//  }
-
-  motor_input = constrain(motor_input,75,105);
-
-
-  myservo.write(motor_input);
-}
-
-float ReadPot1(){
-  float val = analogRead(Pot1);
-  return val;
-}
-
-float ReadPot2(){
-  float val = analogRead(Pot2);
-  return val;
-}
-
-float ReadPot3(){
-  float val = analogRead(Pot3);
-  return val;
+  if (now - last_step_time >= step_interval) {
+    step_control ^= 1;
+    setpoint = step_control ? setpoint_1 : setpoint_2;
+    last_step_time = now;
+  }
 }
